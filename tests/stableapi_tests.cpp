f615a2709741ad9387700d2acbce7eb727049672
Andrew Walker <walker.ab@gmail.com>
2018-03-12 16:01:41 +1100

    Travis support and improved testing

--- tests/stableapi_tests.cpp
+++ tests/stableapi_tests.cpp
@@ -1,7 +1,7 @@
 extern "C" {
 #include "dubins.h"
 }
-
+#include <math.h>
 #include "gtest/gtest.h"
 
 class DubinsTests : public ::testing::Test
@@ -83,11 +83,30 @@ TEST_F(DubinsTests, segmentLengths)
     DubinsPath path;
     int err = dubins_path(&path, q0, q1, turning_radius, LSL);
     ASSERT_EQ(err, EDUBOK);
+    ASSERT_DOUBLE_EQ(dubins_segment_length(&path, -1), INFINITY);
     ASSERT_DOUBLE_EQ(dubins_segment_length(&path, 0), 0.0);
     ASSERT_DOUBLE_EQ(dubins_segment_length(&path, 1), 4.0);
     ASSERT_DOUBLE_EQ(dubins_segment_length(&path, 2), 0.0);
+    ASSERT_DOUBLE_EQ(dubins_segment_length(&path, 3), INFINITY);
 
 }
+
+TEST_F(DubinsTests, SegmentLengthNormalized)
+{
+    configure_inputs(0.0, 0.0, 4.0);
+
+    // find the parameters for a single Dubin's word
+    DubinsPath path;
+    int err = dubins_path(&path, q0, q1, turning_radius, LSL);
+    ASSERT_EQ(err, EDUBOK);
+    ASSERT_DOUBLE_EQ(dubins_segment_length_normalized(&path, -1), INFINITY);
+    ASSERT_DOUBLE_EQ(dubins_segment_length_normalized(&path, 0), 0.0);
+    ASSERT_DOUBLE_EQ(dubins_segment_length_normalized(&path, 1), 4.0);
+    ASSERT_DOUBLE_EQ(dubins_segment_length_normalized(&path, 2), 0.0);
+    ASSERT_DOUBLE_EQ(dubins_segment_length_normalized(&path, 3), INFINITY);
+
+}
+
 
 TEST_F(DubinsTests, sample)
 {
@@ -133,9 +152,9 @@ int nop_callback(double q[3], double t, void* data)
     return 0;
 }
 
-TEST_F(DubinsTests, sampleMany)
+TEST_F(DubinsTests, sampleManyLSL)
 {
-    configure_inputs(0.0, 0.0, 4.0);
+    configure_inputs(M_PI/2,-M_PI/2, 4.0);
 
     // find the parameters for a single Dubin's word
     DubinsPath path;
@@ -144,6 +163,41 @@ TEST_F(DubinsTests, sampleMany)
     err = dubins_path_sample_many(&path, 1.0, &nop_callback, NULL);
 }
 
+TEST_F(DubinsTests, sampleManyRSR)
+{
+    configure_inputs(M_PI/2,-M_PI/2, 4.0);
+
+    // find the parameters for a single Dubin's word
+    DubinsPath path;
+    int err = dubins_path(&path, q0, q1, turning_radius, RLR);
+    ASSERT_EQ(err, EDUBOK);
+    err = dubins_path_sample_many(&path, 1.0, &nop_callback, NULL);
+}
+
+int out_out_early_callback(double q[3], double t, void* data) 
+{
+    int& value = *((int*)data);
+    if( value > 2 ) {
+        return 1;
+    }
+    value += 1;
+    return 0;
+}
+
+TEST_F(DubinsTests, sampleManyOptOutEarly)
+{
+    configure_inputs(0.0, 0.0, 4.0);
+
+    // find the parameters for a single Dubin's word
+    DubinsPath path;
+    int err = dubins_path(&path, q0, q1, turning_radius, LSL);
+    ASSERT_EQ(err, EDUBOK);
+
+    int count = 0;
+    err = dubins_path_sample_many(&path, 1.0, &out_out_early_callback, (void*)(&count));
+    ASSERT_EQ(err, 1);
+    ASSERT_EQ(count, 3);
+}
 
 TEST_F(DubinsTests, pathType)
 {
@@ -176,9 +230,39 @@ TEST_F(DubinsTests, endPoint)
     ASSERT_NEAR(qsamp[2], q1[2], 1e-8);
 }
 
-
-int main(int argc, char **argv) 
+TEST_F(DubinsTests, extractSubpath)
 {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    configure_inputs(0.0, 0.0, 4.0);
+
+    // find the parameters for a single Dubin's word
+    DubinsPath path;
+    int err = dubins_path(&path, q0, q1, turning_radius, LSL);
+    ASSERT_EQ(err, EDUBOK);
+
+    DubinsPath subpath;
+    err = dubins_extract_subpath(&path, 2.0, &subpath);
+    ASSERT_EQ(err, 0);
+
+    double qsamp[3];
+    err = dubins_path_endpoint(&subpath, qsamp);
+    ASSERT_EQ(err, EDUBOK);
+    ASSERT_NEAR(qsamp[0], 2.0, 1e-8);
+    ASSERT_NEAR(qsamp[1], 0.0, 1e-8);
+    ASSERT_NEAR(qsamp[2], 0.0, 1e-8);
 }
+
+TEST_F(DubinsTests, extractInvalidSubpath)
+{
+    configure_inputs(0.0, 0.0, 4.0);
+
+    // find the parameters for a single Dubin's word
+    DubinsPath path;
+    int err = dubins_path(&path, q0, q1, turning_radius, LSL);
+    ASSERT_EQ(err, EDUBOK);
+
+    DubinsPath subpath;
+    err = dubins_extract_subpath(&path, 8.0, &subpath);
+    ASSERT_NE(err, 0);
+}
+
+
